da55f89830a1bdd04ce5032b481c6115a05c0e86
Melissa Mendez <mmmeldz@gmail.com>
2018-08-27 13:20:17 -0400

    Create new rooms

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
 
- class RoomList extends Component {
+ export class RoomList extends Component {
   constructor(props) {
     super(props)
-     this.state = {
-      rooms: [],
-    }
+     this.state = {title: "", rooms: []};
 
 this.roomsRef = this.props.firebase.database().ref('rooms');
+this.handleChange = this.handleChange.bind(this);
+this.createRoom = this.createRoom.bind(this);
+}
+
+handleChange(e) {
+  this.setState({ title: e.target.value });
+}
+
+createRoom(e) {
+  e.preventDefault();
+  this.roomsRef.push({ title: this.state.title });
+  this.setState({ title: "" });
 }
 
 componentDidMount() {
@@ -19,13 +29,23 @@ componentDidMount() {
    }
 
    render() {
+     const roomForm =(
+       <form onSubmit={this.createRoom}>
+       <input type="text" value={this.state.title} placeholder="Enter room name" onChange={this.handleChange}/>
+       <input type="submit" value="create"/>
+       </form>
+     );
+
     const roomList = this.state.rooms.map((room) =>
-      <li key={room.key}>{room.name}</li>
+      <li key={room.key}>{room.title}</li>
     );
+
     return(
-      <ul>{roomList}</ul>
+      <div>
+        <div>{roomForm}</div>
+        <ul>{roomList}</ul>
+      </div>
     );
   }
 }
-
 export default RoomList;
